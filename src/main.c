e9ed19c94ce70459a3fb8ec98b98bb139c9b1d2e
Your Name <you@example.com>
2025-03-19 16:14:32 +0700

    test thôi nhen

--- src/main.c
+++ src/main.c
@@ -1,9 +1,13 @@
- #include<stdio.h>
- #include<string.h>
- #include<gtk/gtk.h>
+ #include <stdio.h>
+ #include <string.h>
+ #include <gtk/gtk.h>
+ #include "menu.h"
+ #include "orders.h"
+ #include "employees.h"
+ #include "stats.h"
  
  int main(){
- 	printf("huy");
+
  	return 0;
  }
  
